0a3a850fb285f40f5fae81d9e0a8ba179eedccde
Tigges, Matthew <43684926+Thewtus@users.noreply.github.com>
2024-04-05 16:29:20 -0400

    final

--- 2D Idle Game Prototype/Assets/Scripts/GameManager.cs
+++ 2D Idle Game Prototype/Assets/Scripts/GameManager.cs
@@ -12,25 +12,60 @@ public class GameManager : MonoBehaviour
     public float costMulti = 25;
     public TextMeshProUGUI multiTxt;
     public TextMeshProUGUI multiButtonTxt;
-    public ParticleSystem part;
+    public ParticleSystem[] part;
+    public float ptsMiner = 1;
+    public float mineUpgrades = 0;
+    public TextMeshProUGUI ptsMinerTxt;
+    public TextMeshProUGUI minerButtonTxt;
+    public GameObject minerImage;
+    public float costMiner = 15;
+
+    public TextMeshProUGUI objTxt;
+    public TextMeshProUGUI objButtonTxt;
+    public float objTimer = 300;
+    public float costObj = 200;
+    bool isGameOver = false;
+    public TextMeshProUGUI GOTxt;
+    private static float MAX_TIMER;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MAX_TIMER = objTimer;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if(objTimer > 0)
+        {
+            objTimer -= Time.deltaTime;
+            objTxt.text = Mathf.RoundToInt(objTimer).ToString();
+        }
+        else
+        {
+            gameOver();
+        }
         //do passive generation upgrade
+        if(ptsMiner > 0 && mineUpgrades > 0)
+        {
+            ptsMiner -= Time.deltaTime;
+        }
+        else if (ptsMiner <= 0 && !isGameOver) 
+        {
+            points += 1;
+            SpriteRenderer sr = minerImage.GetComponent<SpriteRenderer>();
+            sr.flipX = !sr.flipX;
+            part[1].Play();
+            ptsMiner = 1 / mineUpgrades;
+        }
+        scoreTxt.text = "LUDDIES: " + Mathf.CeilToInt(points) + "L";
     }
 
     public void Clicked()
     {
         points+= earnAmt * multi;
-        scoreTxt.text = "LUDDIES: " + Mathf.CeilToInt(points) + "L";
-        part.Play();
+        part[0].Play();
     }
 
     public void MultiplierUpgrade()
@@ -38,10 +73,48 @@ public class GameManager : MonoBehaviour
         if(points >= costMulti)
         {
             points -= costMulti;
-            multi += 0.05f;
-            costMulti *= multi + .1f;
-            multiTxt.text = "Multiplier: " + multi + "x";
+            multi += 0.1f;
+            costMulti *= multi + .15f;
+            multiTxt.text = "ROBBIE MULTIPLIER: " + multi + "x";
             multiButtonTxt.text = "Upgrade Multiplier\n COST: " + Mathf.RoundToInt(costMulti) + "L";
+        }
+    }
+
+    public void MinerUpgrade()
+    {
+        if( points >= costMiner)
+        {
+            points -= costMiner;
+            costMiner *= 2 + (.05f * mineUpgrades);
+            if (mineUpgrades == 0)
+            {
+                minerImage.gameObject.SetActive(true);
+                ptsMinerTxt.gameObject.SetActive(true);
+            }
+            mineUpgrades++;
+            ptsMinerTxt.text = "Auto Miner: " + mineUpgrades + "L/s";
+            minerButtonTxt.text = "Upgrade Multiplier\n COST: " + Mathf.RoundToInt(costMiner) + "L";
+        }
+        
+
+    }
+
+    private void gameOver()
+    {
+        isGameOver = true;
+        GOTxt.gameObject.SetActive(true);
+        multiButtonTxt.GetComponentInParent<GameObject>().SetActive(false);
+        minerButtonTxt.GetComponentInParent<GameObject>().SetActive(false);
+    }
+
+    public void ExtendObjective()
+    {
+        if(points >= costObj)
+        {
+            points -= costObj;
+            objTimer = MAX_TIMER;
+            costObj *= 2.5f;
+            objButtonTxt.text = "EXTEND ASSIGNMENT\nCOST: " + Mathf.RoundToInt(costObj) + "L";
         }
     }
 }
